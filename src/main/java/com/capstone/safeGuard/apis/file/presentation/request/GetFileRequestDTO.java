3eabd59d0c6ea72828d69b400d2997ec838fc4f3
LeeShinHaeng <dragon11796@gmail.com>
2024-11-04 10:16:28 +0900

    refactor: file record DTO 사용하도록 변경

--- src/main/java/com/capstone/safeGuard/apis/file/presentation/request/GetFileRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.safeGuard.apis.file.presentation.request;
-
-import lombok.Getter;
-
-@Getter
-public class GetFileRequestDTO {
-    private String userId;
-    private String userType;
-}
