2049201f007bd30b3c885ed4363d0d5183a25923
LeeShinHeang <dragon11796@gmail.com>
2025-01-26 14:20:52 +0900

    feat: Notice 도메인 Controller Map DTO로 변경

--- src/main/java/com/capstone/safeGuard/apis/comment/presentation/response/CommentResponse.java
+++ src/main/java/com/capstone/safeGuard/apis/comment/presentation/response/CommentResponse.java
@@ -8,9 +8,11 @@ public record CommentResponse(
 	String commentator,
 	String commentDate
 ) {
-	public CommentResponse(String content, String commentator, String commentDate) {
-		this.content = content;
-		this.commentator = commentator;
-		this.commentDate = commentDate;
+	public static CommentResponse of(String content, String commentator, String commentDate) {
+		return CommentResponse.builder()
+			.content(content)
+			.commentator(commentator)
+			.commentDate(commentDate)
+			.build();
 	}
 }
